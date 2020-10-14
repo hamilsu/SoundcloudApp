eecca13852c1b89ee2ec9be00d904cfb7ca124e9
Storm Hamilton <hamilsu@mail.uc.edu>
2020-10-14 18:06:03 -0400

    Added Maven support and a number of dependencies including springboot

---
+++ src/main/java/com/statcloud/app/StatcloudApplication.java
@@ -0,0 +1,15 @@
+package com.statcloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatcloudApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(StatcloudApplication.class, args);
+        }
+
+    }
+
+
